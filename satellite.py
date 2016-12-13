bc6aab815b85557264a9c4c43c2f15b13ba67ae0
pacozaa <pacosarin@gmail.com>
2016-12-13 17:56:20 +0700

    [Add] satellite calculation

--- satellite.py
+++ satellite.py
@@ -1,23 +1,105 @@
-# -*- coding: utf-8 -*-
+from __future__ import print_function   # PEP 3105: Make print a function
+
+import math
+import time
+from datetime import datetime, timedelta
+import sys
+import os.path
+import ephem
+import numpy as np
+import matplotlib.pyplot as plt
+import zipfile
+import geocoder
+
+try:
+    from urllib.request import URLopener
+except ImportError:
+    from urllib import URLopener
+
+try:
+    input = raw_input
+except NameError:
+    pass
 
 class satellite(spaceObject):
 
     #Constructor
     def __init__(self):
         super(satellite, self).__init__()
-        self.TLE = ""
+        self.tleSource = [
+                            {
+                             'name':  'Default',
+                             'file':  'tle.txt',
+                            },
+                           ]
+        self.saveSatellite = ""
         self.lattitude = 0
         self.longtitude = 0
-    
+        self.home = ephem.Observer()
+
     #Getter Method
-    def getTLE(self):
-        return self.TLE
-        
-    def getALT(self, date):
+    def getTLE(self,id):
         return None
-        
+
+    def getALT(self, id, date):
+        satelligeInfo = self.saveSatellite[id]
+        self.home.date = datetime.utcnow()
+        satelligeInfo['body'].compute(home)
+        alt = satelligeInfo['body']
+
+        return alt
+
+
     def convertToXYZ(self, alt):
         return None
-        
-    def setTLE(self, newTLE):
-        self.TLE = newTLE
+
+    def setTLE(self, newTLE=None):
+        self.saveSatellite =  self.processTLEdata(self.tleSource)
+
+    def readTLEFile(source):
+        ''' Read a TLE file (unzip if necessary) '''
+        sourceName = source['name']
+        sourceFile = source['file']
+
+        tempContent = []
+        with open(sourceFile) as f:
+            for aline in f:
+                tempContent.append(aline.replace('\n', ''))
+            print(len(tempContent) // 3,
+                  'TLEs loaded from {}'.format(sourceFile))
+
+        return tempContent
+
+    def processTLEdata(tleSource):
+        ''' Process each TLE entry '''
+        sats = []
+        for source in tleSource:
+            print("Processing {}".format(source['name']))
+            tempContent = self.readTLEfile(source=source)
+            print()
+            if tempContent:
+                i_name = 0
+                while 3 * i_name + 2 <= len(tempContent):
+                    rawTLEname = tempContent[3 * i_name + 0]
+                    rawTLEdat1 = tempContent[3 * i_name + 1]
+                    rawTLEdat2 = tempContent[3 * i_name + 2]
+                    partsTLEdat1 = rawTLEdat1.split()
+                    try:
+                        body = ephem.readtle(rawTLEname, rawTLEdat1, rawTLEdat2)
+                    except ValueError:
+                        print("Error: line does not conform to tle format")
+                        print("       " + rawTLEname)
+                        print("       " + rawTLEdat1)
+                        print("       " + rawTLEdat2)
+                        print()
+                    else:
+                        name = body.name
+                        number = partsTLEdat1[1]
+                        designator = partsTLEdat1[2]
+                        sats.append({'name': name,
+                                     'number': number,
+                                     'designator': designator,
+                                     'color': source['color'],
+                                     'body': body, })
+                    i_name += 1
+        return sats
